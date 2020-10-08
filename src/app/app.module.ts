4cde3807a051d15d8204d913591eeae4b07736f4
Subash Khatri <subasskhatrii@gmail.com>
2020-10-08 15:27:10 +0545

    - added Todo by creating add function   - created update and delate function   - added checked and unchecked function to the project   - added remaing function which shows the remaining todos   - added clear function to clear the completed function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { FormsModule } from '@angular/forms';
+import { AutofocusFixModule } from 'ngx-autofocus-fix'; // <--- new code
 
 @NgModule({
   declarations: [
@@ -12,7 +15,10 @@ import { TodoListComponent } from './components/todo-list/todo-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AutofocusFixModule.forRoot(), // <--- new code
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,34 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Todo } from '../../interfaces/todo'
 @Component({
   selector: 'todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
-  todos: object[];
+  todos: Todo[];
+  todoTitle: string;
+  idForTodo: number;
+  beforeEditCache: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.beforeEditCache = '';
+    this.idForTodo = 4;
+    this.todoTitle = '';
     this.todos = [
       {
         'id': 1,
-        'title': 'Complete Angular Project Today',
+        'title': 'Yoga in the Morning',
         'completed': false,
         'editing': false,
 
       },
       {
         'id': 2,
-        'title': 'Hurry up and cook food',
+        'title': 'Start my music therapy',
         'completed': false,
         'editing': false,
 
       },
       {
         'id': 3,
-        'title': 'Wash dishes',
+        'title': 'start coding in angular',
         'completed': false,
         'editing': false,
 
@@ -37,5 +43,53 @@ export class TodoListComponent implements OnInit {
     ]
 
   }
+  addTodo(): void {
+    if (this.todoTitle.trim().length === 0){
+      return;
+    }
+    this.todos.push({
+      id: this.idForTodo,
+      title: this.todoTitle,
+      completed: false,
+      editing: false
+    })
+    this.todoTitle = '';
+    this.idForTodo++;
+  }
+
+  editTodo(todo: Todo): void {
+    this.beforeEditCache = todo.title;
+    todo.editing = true;
+
+  }
+  doneEdit(todo: Todo): void{
+    if (todo.title.trim().length === 0){
+      todo.title = this.beforeEditCache;
+    }
+    todo.editing = false;
+  }
+  cancelEdit(todo: Todo): void{
+    todo.title = this.beforeEditCache;    
+    todo.editing = false;
+  }
+
+
+  deleteTodo(id: number): void{
+    this.todos = this.todos.filter(todo => todo.id !== id);
+
+  }
+  remaining(): number {
+    return this.todos.filter(todo => !todo.completed).length;
+  }
+
+  atLeastOneCompleted(): boolean {
+    return this.todos.filter(todo => todo.completed).length > 0;
+  }
+  clearCompleted(): void{
+    this.todos = this.todos.filter(todo => !todo.completed);
+  }
+
 
 }
+
+
